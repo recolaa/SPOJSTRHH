e888c68105174616cc55f0c3802b1e5da78d67e1
Haden Fowler <fowlerhaden1988@gmail.com>
2025-01-23 23:57:10 -0600

    Added comments

--- main.cpp
+++ main.cpp
@@ -4,16 +4,16 @@
 using namespace std;
 
 int main() {
-    int t;
-    string c;
-    cin >> t;
-    for(int i = 0; i < t; i++){
-        cin >> c;
-        cout << c[0];
-        for(int i = 2; i < (c.size()/2); i+=2){
-            cout << c[i];;
+    int t; //number of test cases
+    string c; //string to be read
+    cin >> t; //read number of test cases
+    for(int i = 0; i < t; i++){ //loop through test cases
+        cin >> c; //read string
+        cout << c[0]; //print first character
+        for(int i = 2; i < (c.size()/2); i+=2){ //loop through string
+            cout << c[i];; //print every other character
         }
-        cout << endl;
+        cout << endl; //print new line
     }
 } 
 
